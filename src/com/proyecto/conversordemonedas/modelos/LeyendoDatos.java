dd58f615e1ee02efba07e7feacebf79045d16e82
Luis Miguel <luismy852@gmail.com>
2024-10-06 22:42:09 -0500

    Se agrego la funcionalidad de convertir los datos a objetos de tipo Moneda y se añaden a una lista para poder ser accedidos despues de que se termine la funcion.

--- src/com/proyecto/conversordemonedas/modelos/LeyendoDatos.java
+++ src/com/proyecto/conversordemonedas/modelos/LeyendoDatos.java
@@ -1,16 +1,37 @@
 package com.proyecto.conversordemonedas.modelos;
 
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class LeyendoDatos {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private List<Moneda> lista = new ArrayList<>();
+
+    public List<Moneda> getLista() {
+        return lista;
+    }
 
     public void guardarDatos (String datos){
         DatosMoneda datosMoneda = gson.fromJson(datos, DatosMoneda.class);
+
+        Map<String, Double> tasasDeCambio = datosMoneda.conversion_rates();
+
+        for (Map.Entry<String, Double> entrada : tasasDeCambio.entrySet()) {
+            String codigoMoneda = entrada.getKey();
+            double tasaConversion = entrada.getValue();
+            Moneda moneda = new Moneda(codigoMoneda, tasaConversion);
+            lista.add(moneda);
+            System.out.println(moneda);
+
+        }
+
         System.out.println(datosMoneda);
+        System.out.println(lista.size());
+
     }
 
 }
