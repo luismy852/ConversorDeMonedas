c5f63dacfe565161c6df181a104259eb41b36c0d
Luis Miguel <luismy852@gmail.com>
2024-10-06 23:09:36 -0500

    Se agregan Getter y Setter, se sobrescribre el metodo toString y se agrega un contructor.

--- src/com/proyecto/conversordemonedas/modelos/Moneda.java
+++ src/com/proyecto/conversordemonedas/modelos/Moneda.java
@@ -2,5 +2,31 @@ package com.proyecto.conversordemonedas.modelos;
 
 public class Moneda {
     private String codigoMoneda;
-    private int valor;
+    private double valor;
+
+    public String getCodigoMoneda() {
+        return codigoMoneda;
+    }
+
+    public void setCodigoMoneda(String codigoMoneda) {
+        this.codigoMoneda = codigoMoneda;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Moneda(String codigoMoneda, double tasaConversion) {
+        this.codigoMoneda = codigoMoneda;
+        this.valor = tasaConversion;
+    }
+
+    @Override
+    public String toString() {
+        return "La moneda es " + codigoMoneda + " y su valor es " + valor ;
+    }
 }
